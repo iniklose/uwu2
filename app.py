fb694577258988d7460f98ddcc67fbb0ccde96a3
selehngendi <selehngendi@outlook.com>
2026-04-12 23:54:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 # ==========================================
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 API_URL = "api.fireworks.ai/inference/v1/chat/completions"
-MODEL_AI = "accounts/fireworks/models/kimi-k2p5"
+MODEL_AI = "accounts/fireworks/models/gpt-oss-20b"
 WALLET_KEY = os.getenv("WALLET_KEY")
 
 # FREE TIER SETTINGS
